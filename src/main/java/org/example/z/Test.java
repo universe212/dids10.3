52da23a52fbbb7597c970bee6290be107b378bf3
liunian <321585854@qq.com>
2024-10-03 15:40:19 +0800

    10.3 test1

---
+++ src/main/java/org/example/z/Test.java
@@ -0,0 +1,13 @@
+package org.example.z;
+
+/**
+ * ClassName: Test
+ * Package: org.example.z
+ * Description
+ *
+ * @Author HuanZ
+ * @Create 2024/10/3 15:39
+ * @Version 1.0
+ */
+public class Test {
+}
